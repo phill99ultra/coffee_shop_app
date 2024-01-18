49683617a323329f08143f59e7f66a2e33167260
Filip Crijanovschi <filipcrijanovschi@Filips-MacBook-Air.local>
2024-01-19 01:31:46 +0200

    Add splash screen for android.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Platform } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import TabNavigator from './src/navigators/TabNavigator';
 import PaymentScreen from './src/screens/PaymentScreen';
@@ -10,6 +12,11 @@ import { RootStackParamList } from './src/types/navigation';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
